3e1d36281a19cc7c47a883907065dfa6beec93af
Synapse B2B <dev@synapseb2b.com>
2025-10-02 15:15:26 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,12 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import WhatsAppButton from './WhatsAppButton';
+import Cursor from './Cursor'; // Importa o novo componente
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Cursor /> {/* Adiciona o cursor customizado à página */}
       <Navbar />
       <WhatsAppButton /> 
       <main>
